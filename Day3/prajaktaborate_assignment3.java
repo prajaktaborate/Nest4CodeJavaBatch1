3634de6f0efd970d74eafa1fa7647c97ecd78d94
prajaktaborate <65675588+prajaktaborate@users.noreply.github.com>
2020-08-19 23:56:07 +0530

    Update prajaktaborate_assignment3.java

--- Day3/prajaktaborate_assignment3.java
+++ Day3/prajaktaborate_assignment3.java
@@ -8,17 +8,17 @@ public class assignment3 {
 		// TODO Auto-generated method stub
           Scanner scan = new Scanner(System.in);
 		
-        System.out.println("Enter first string");                   //
+        System.out.println("Enter first string");                   // User enter first string//
         String s1= scan.next();
         
-        System.out.println("Enter second string");                   //
+        System.out.println("Enter second string");                   //User enter second string//
         	String s2= scan.next();
         
-        	String St2 = (s1.substring(2))+(s1.substring(0,2));      //
-        	String St1 = (s2.substring(2))+(s2.substring(0,2));
+        	String St2 = (s1.substring(2))+(s1.substring(0,2));      //correcting second string//
+        	String St1 = (s2.substring(2))+(s2.substring(0,2));      //correcting first string//  
 
-        	System.out.println("Correct first string is :" + St1);    //    
-        	System.out.println("Correct second string is :"+ St2);     //
+        	System.out.println("Correct first string is :" + St1);    // printing first corrected string// 
+        	System.out.println("Correct second string is :"+ St2);     //printing second corrected string//
 
         	
 	}

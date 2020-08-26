9a791f6b558da8c78a851af641e588b6a804ebf1
prajaktaborate <65675588+prajaktaborate@users.noreply.github.com>
2020-08-26 23:57:42 +0530

    Add files via upload

---
+++ Day7/ConstructObjects.java
@@ -0,0 +1,13 @@
+package Java_day1_package;
+
+public class ConstructObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Parrot parrot = new Parrot();
+        parrot.fly();
+        Ostrich ostrich = new Ostrich();
+        ostrich.fly();
+	}
+
+}

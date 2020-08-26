f6f56652daa7923ad6ebfba1c8892facfd66a304
prajaktaborate <65675588+prajaktaborate@users.noreply.github.com>
2020-08-26 23:56:56 +0530

    Add files via upload

---
+++ Day7/Ostrich.java
@@ -0,0 +1,19 @@
+package Java_day1_package;
+
+public class Ostrich extends Bird{
+	
+	public void fly() {
+	      System.out.println("Ostrich cannot fly");
+	}
+	     
+		      @Override
+		      public void swim() {
+		    	  System.out.println("Ostrich cannot swim");
+		    	  
+		      }
+}	
+
+
+
+
+
